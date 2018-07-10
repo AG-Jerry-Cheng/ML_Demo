83cefe071eed3649d91cd12045854b7c8b5e207e
jerrycheng <jerry.cheng@autogravity.com>
2018-07-10 14:18:31 -0700

    cams ml example

---
+++ cams_ml_example.py
@@ -0,0 +1,86 @@
+import numpy
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn import preprocessing
+from keras.models import Sequential
+from keras.layers import Dense,Dropout
+numpy.random.seed(10)
+
+def PreprocessData(raw_df):
+    df = raw_df
+    x_OneHot_df = pd.get_dummies(data=df, columns=["fico_band", "platform"])
+    #x_OneHot_df = pd.get_dummies(data=df, columns=["fico_band"])
+
+    ndarray = x_OneHot_df.values
+    Features = ndarray[:, 1:]
+    print(Features)
+    Label = ndarray[:, 0]
+    minmax_scale = preprocessing.MinMaxScaler(feature_range=(0, 1))
+    scaledFeatures = minmax_scale.fit_transform(Features)
+
+    return scaledFeatures, Label
+
+
+def show_train_history(train_history,train,validation):
+    plt.plot(train_history.history[train])
+    plt.plot(train_history.history[validation])
+    plt.title('Train History')
+    plt.ylabel(train)
+    plt.xlabel('Epoch')
+    plt.legend(['train', 'validation'], loc='upper left')
+    plt.show()
+
+
+if __name__ == "__main__":
+    all_df = pd.read_excel("data/cams_data_full.xls")
+    cols=['result', 'fico_score', 'platform', 'down_payment', 'loan_amount', 'fico_band', 'age', 'year',
+          'monthly_income', 'estimated_monthly_payment', 'payment_to_income', 'loan_to_value',
+          'bankruptcies_on_record', 'debt_to_income_ratio', 'current_delinquencies', 'vehicle_msrp']
+    all_df = all_df[cols]
+    msk = numpy.random.rand(len(all_df)) < 0.8
+    train_df = all_df[msk]
+    test_df = all_df[~msk]
+    print('total:', len(all_df),
+          'train:', len(train_df),
+          'test:', len(test_df))
+
+    train_Features, train_Label = PreprocessData(train_df)
+    test_Features, test_Label = PreprocessData(test_df)
+    print(train_Features)
+
+    model = Sequential()
+
+    # create input layer
+    model.add(Dense(units=40, input_dim=21,
+                    kernel_initializer='uniform',
+                    activation='relu'))
+
+    # create hidden layer
+    model.add(Dense(units=30,
+                    kernel_initializer='uniform',
+                    activation='relu'))
+
+    model.add(Dense(units=1,
+                    kernel_initializer='uniform',
+                    activation='sigmoid'))
+
+    # create output layer
+    model.compile(loss='binary_crossentropy',
+                  optimizer='adam', metrics=['accuracy'])
+
+    print(train_Features)
+
+    train_history = model.fit(x=train_Features,
+                              y=train_Label,
+                              validation_split=0.1,
+                              epochs=30,
+                              batch_size=30, verbose=2)
+
+    show_train_history(train_history, 'acc', 'val_acc')
+
+    show_train_history(train_history, 'loss', 'val_loss')
+
+    scores = model.evaluate(x=test_Features,
+                            y=test_Label)
+
+    print (scores[1])
